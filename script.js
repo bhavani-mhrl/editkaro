20780a4ccf570aa90060d79a9016c2bcd4fe7079
bhavani-mhrl <mchrlbhavani@gmail.com>
2025-11-23 20:31:23 +0530

    editkaro

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ let moon=document.querySelector(".fa-moon");
 let body=document.querySelector("body");
 let header=document.querySelector("header");
 let logo=document.querySelector(".image-logo");
-let links=document.querySelectorAll(".header-link")
-moon.addEventListener("click",()=>{
+let links=document.getElementsByClassName(".der-link")
+var innerpart=document.querySelectorAll(".inner-part");
+let condition=true;
+    moon.addEventListener("click",()=>{
+        if(condition){
     console.log("clicked");
     moon.classList.toggle("fa-sun");
     moon.classList.toggle("fa-moon")
     body.classList.toggle("dark-mode");
     header.classList.toggle("header-dark");
-    // innerpart.forEach(function(el){
-    // el.classList.toggle('inner-partforlightmode'); 
-    // });
+    innerpart.forEach(function(el){
+    el.classList.toggle('inner-partforlightmode'); 
+    });
     logo.src="dark-mode-logo.png";
-    // links.classList.toggle("hdr-link");
+    condition=false;
+        }
+        else{
+    logo.src="editkaro-logo.png"
+    moon.classList.toggle("fa-sun");
+    moon.classList.toggle("fa-moon")
+    body.classList.toggle("dark-mode");
+    header.classList.toggle("header-dark");
+    condition=true;
+}
 });
+
+
 
 
 let menu_icon=document.querySelector(".fa-bars");
